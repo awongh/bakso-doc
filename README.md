1fb98b1d26c231967476d9da289a56a2c6d7842f
awongh <akira@awongh.com>
2022-05-02 01:09:23 +0800

    take out queue and upload

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Clone the repo.
 Start the Bakso Docs server:
 
 ```
-NO_QUEUE=true node src/index.js
+node src/index.js
 ```
 
 Make a request to save a file to the repo directory:
@@ -22,40 +22,10 @@ curl -X POST -H "Content-Type: application/json" --output test.pdf -d @test/11-1
 
 ### Start Express
 ```
-NO_QUEUE=false node src/index.js
-```
-
-### Start Queue Worker
-```
-node src/worker.js
-```
-
-### Start Redis
-```
-redis-server /usr/local/etc/redis.conf
-```
-
-### Queue a Job
-
-The entire body of this request will be passed back to the hook when the job is completed.
-
-```
-curl -X POST -H "Content-Type: application/json" -d @test/11-17-poster-params.json http://localhost:5000/job
+node src/index.js
 ```
 
 ### Test PDF Output without Queue - Save a File using CURL
 ```
 curl -X POST -H "Content-Type: application/json" --output test.pdf -d @test/11-17-poster-params.json http://localhost:5000/download
-```
-
-### Test Google Cloud Upload File Hook
-
-Start the hook endpoint test server
-```
-node test/test-hook-endpoint.js
-```
-
-Create the uploads folder
-```
-mkdir tmp
 ```
--- src/index.js
+++ src/index.js
@@ -1,41 +1,12 @@
 require('dotenv').config()
 const express = require('express');
-const bullmq = require('bullmq');
-const { Queue, QueueEvents } = bullmq;
 const pdf = require('./pdf.js');
-
-const QUEUE_NAME = process.env.QUEUE_NAME || 'work';
 
 // Serve on PORT on Heroku and on localhost:5000 locally
 const PORT = process.env.PORT || '5000';
 
-// Connect to a local redis instance locally, and the Heroku-provided URL in production
-const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
-
 const app = express();
 app.use(express.json())
-
-if(process.env.NO_QUEUE == "false"){
-
-  // Create / Connect to a named work queue
-  const workQueue = new Queue(QUEUE_NAME, REDIS_URL);
-
-  /*
-   * ========================================================
-   *  Listen for queue events and log them here
-   * ========================================================
-   */
-
-  const queueEvents = new QueueEvents(QUEUE_NAME);
-
-  queueEvents.on('completed', ({ jobId }) => {
-    console.log('done painting');
-  });
-
-  queueEvents.on('failed', (j) => {
-    console.error('error painting', j);
-  });
-}
 
 /*
  * ========================================================
@@ -44,12 +15,6 @@ if(process.env.NO_QUEUE == "false"){
  * ========================================================
  * ========================================================
  */
-
-// Kick off a new job by adding it to the work queue
-app.post('/job', async (req, res) => {
-  const job = workQueue.add(QUEUE_NAME, req.body,{ priority: req.body.priority});
-  res.json({ id: job.id });
-});
 
 app.post('/download', async (req, res) => {
   const file = await pdf(req.body.pdfParams);
