5c476151f69e565a35bba1702b075158f7e40e5a
awongh <akira@awongh.com>
2022-06-03 18:16:18 +0200

    add security hash measure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const retry = require('async-retry');
 const express = require('express');
+const crypto = require('crypto');
 const pdf = require('./pdf.js');
 
 
 // Serve on PORT on Heroku and on localhost:5000 locally
 const PORT = process.env.PORT || '5500';
+const KEY = process.env.SECRET_KEY || 'hello world';
 
 const app = express();
 app.use(express.json())
@@ -18,8 +20,18 @@ app.use(express.json())
  * ========================================================
  */
 
+function isKeyValid(key){
+  var hash = crypto.createHash('sha512');
+  data = hash.update(KEY, 'utf-8');
+  return data.digest('hex') === key;
+}
+
 app.post('/download', async (req, res) => {
   try{
+
+    // exit out if key doesn't match
+    if(isKeyValid(req.body.key) === false) throw new Error;
+
     const file = await retry(
       async (bail, count) => {
         console.log(`tried ${count} times`);
